6ae2aaa5f9a2dddc9a4797e0de16f78b426c7882
ibqn <evgen.ibqn@gmail.com>
2023-12-10 14:58:44 +0100

    animation to about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,19 +1,41 @@
+'use client'
+
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { Stats } from '@/components/stats'
+import { motion, useInView } from 'framer-motion'
+import { FadeDirection, fadeIn } from '@/utils/variants'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 type Props = {}
 
 export const About = (props: Props) => {
+  const ref = useRef(null)
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  const isInView = useInView(ref, { amount: isMobile ? 0.0 : 0.5 })
+
   return (
-    <section className="py-12 xl:pb-24 xl:pt-0">
+    <section ref={ref} className="py-12 xl:pb-24 xl:pt-0">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row">
-          <div className="relative flex-1">
+          <motion.div
+            className="relative flex-1"
+            variants={fadeIn(FadeDirection.right, 0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.4 }}
+          >
             <Image src="/about/img2.png" alt="About" width={559} height={721} />
-          </div>
+          </motion.div>
 
-          <div className="xl:max-w-[470px]">
+          <motion.div
+            className="xl:max-w-[470px]"
+            variants={fadeIn(FadeDirection.left, 0.5)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.4 }}
+          >
             <h2 className="h2 mb-[38px]">About Hotely</h2>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
@@ -22,7 +44,7 @@ export const About = (props: Props) => {
               iure?
             </p>
             <div className="my-5 min-h-[35px] xl:my-10">
-              <Stats />
+              {isInView && <Stats />}
             </div>
             <p className="mb-10">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -31,7 +53,7 @@ export const About = (props: Props) => {
             </p>
 
             <Button variant="accent">Explore More</Button>
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
