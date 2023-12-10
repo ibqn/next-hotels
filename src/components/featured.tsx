9728ba05f4fe2dfd4a9f2d222a3d72819bed166e
ibqn <evgen.ibqn@gmail.com>
2023-12-10 13:40:31 +0100

    wip on featured

--- src/components/featured.tsx
+++ src/components/featured.tsx
@@ -1,7 +1,28 @@
+import Image from 'next/image'
+
 type Props = {}
 
 export const Featured = (props: Props) => {
   return (
-    <section className="relative ml-auto max-w-[1305px] rounded-bl-[30px] rounded-tl-[30px] bg-softgreen-secondary px-[80px] py-[60px] xl:-top-[120px] xl:h-[240px]"></section>
+    <section className="relative ml-auto max-w-[1305px] rounded-bl-[30px] rounded-tl-[30px] bg-softgreen-secondary px-[80px] py-[60px] xl:-top-[120px] xl:h-[240px]">
+      <div className="flex h-full flex-col items-center gap-x-[30px] text-center xl:flex-row xl:text-left">
+        <Image
+          className="xl:mr-[50px]"
+          src="/featured/icon.svg"
+          alt="Featured"
+          width={73}
+          height={84}
+        />
+        <h2 className="h2 flex-1 leading-relaxed text-softgreen">
+          Book an awesome room in less than one minute.
+        </h2>
+
+        <p className="flex-1 leading-loose text-softgreen">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus
+          distinctio fuga animi quisquam recusandae, voluptatum eos dolore,
+          laboriosam alias suscipit dolores adipisci exercitationem fugit illum.
+        </p>
+      </div>
+    </section>
   )
 }
